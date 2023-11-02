dec61171bfb689338bfd9a6d98e3b8efc4ad9d11
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-11-02 13:35:25 +0300

    Added the swagger api ui and created the requiremnts.txt

---
+++ app/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
+from models import db, User, Role, Story, Donation, Beneficiary, Organization, Inventory, Reminder
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import secrets
+from flask_swagger_ui import get_swaggerui_blueprint
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mazingira.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    #swagger configs
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "Mazingira Application"
+    }
+    )
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    CORS(app)
+
+    secret_key = secrets.token_hex(16)
+    app.config['SECRET_KEY'] = secret_key
+
+    return app
+
+app = create_app()
+
+# Define a WTForms class for user signup
+class SignupForm(FlaskForm):
+    user_name = StringField('user_name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+# Define a WTForms class for user login
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+@app.route('/')
+def home():
+    return "Welcome to my app"
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+
+    user_name = data.get('user_name')
+    email = data.get('email')
+    password = data.get('password')
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({'message': 'User with this email already exists'}), 409
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(user_name=user_name, email=email, password=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'})
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    return jsonify({'message': 'Login successful'})
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
