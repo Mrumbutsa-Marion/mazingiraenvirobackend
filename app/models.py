407cd5b712e4766b578c58d381d0d972c6c72fbc
MiriamRehema <rehemamiriam30@gmail.com>
2023-11-06 12:18:30 +0300

    added payment

--- app/models.py
+++ app/models.py
@@ -115,6 +115,18 @@ class Reminder(db.Model):
         return f"<Reminder(user_id={self.user_id}, organization_id={self.organization_id})>"
     
 
+# class Payment(db.Model):
+#     __tablename__ = 'payment'
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     donor_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+#     amount = db.Column(db.Numeric(10, 2), nullable=False)
+#     payment_method = db.Column(db.String(50), nullable=False)
+#     date = db.Column(db.Date, nullable=False)
+#     transaction_id = db.Column(db.String(120), unique=True, nullable=True)
+
+#     def __repr__(self):
+#         return f"<Payment(amount={self.amount})>"
 class Payment(db.Model):
     __tablename__ = 'payment'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -124,6 +136,18 @@ class Payment(db.Model):
     payment_method = db.Column(db.String(50), nullable=False)
     date = db.Column(db.Date, nullable=False)
     transaction_id = db.Column(db.String(120), unique=True, nullable=True)
+    status = db.Column(db.String(20), nullable=False)
+    is_anonymous = db.Column(db.Boolean, nullable=False)  
 
     def __repr__(self):
-        return f"<Payment(amount={self.amount})>"
+        return f"<Payment(amount={self.amount}, status='{self.status}', is_anonymous={self.is_anonymous})>"
+
+
+
+
+
+
+
+
+
+
